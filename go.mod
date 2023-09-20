9ca4958892f4bc80e83ae4da4a92c52efb9baf93
Carlos Ribeiro <carlos.ribeiro@ccc.ufcg.edu.br>
2023-09-20 17:59:49 -0300

    improve REST request

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.21.1
 
 require (
 	github.com/go-resty/resty/v2 v2.8.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.15.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/go-resty/resty/v2"
 )
 
 func main() {
-	client := resty.New()
+	godotenv.Load(".env")
 
+	client := resty.New()
 	resp, err := client.R().
     	EnableTrace().
-    	Get("https://api.github.com/octocat")
+		SetQueryParams(map[string]string{
+			"state": "all",
+			"sort": "created",
+			"per_page": "1",
+			"page": "1",
+		}).
+		SetHeader("Accept", "application/vnd.github+json").
+		SetHeader("X-GitHub-Api-Version", "2022-11-28").
+		SetAuthToken(os.Getenv("GITHUB_TOKEN")).
+    	Get("https://api.github.com/repos/neovim/neovim/pulls")
 	
 	if err != nil {
 		fmt.Println(err)
