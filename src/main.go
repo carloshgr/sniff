4b40a08aef40bfef8e354db97ee41eca40cbd0e8
Carlos Ribeiro <carlos.ribeiro@ccc.ufcg.edu.br>
2023-09-21 15:35:33 -0300

    scrape comments from neovim

--- src/main.go
+++ src/main.go
@@ -2,31 +2,42 @@ package main
 
 import (
 	"os"
+	"log"
 	"fmt"
+	"regexp"
+	
 	"github.com/joho/godotenv"
 	"github.com/go-resty/resty/v2"
 )
+
+func send_request(client *resty.Client, page int, owner string, repo string) *resty.Response {
+	resp, err := client.R().
+		EnableTrace().
+		SetQueryParams(map[string]string{
+			"state": "all",
+			"sort": "created",
+			"per_page": "10",
+			"page": fmt.Sprintf("%d", page),
+		}).
+		SetHeader("Accept", "application/vnd.github+json").
+		SetHeader("X-GitHub-Api-Version", "2022-11-28").
+		SetAuthToken(os.Getenv("GITHUB_TOKEN")).
+		Get(fmt.Sprintf("https://api.github.com/repos/%s/%s/pulls/comments", owner, repo))
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return resp
+}
 
 func main() {
 	godotenv.Load(".env")
 
 	client := resty.New()
-	resp, err := client.R().
-    	EnableTrace().
-		SetQueryParams(map[string]string{
-			"state": "all",
-			"sort": "created",
-			"per_page": "1",
-			"page": "1",
-		}).
-		SetHeader("Accept", "application/vnd.github+json").
-		SetHeader("X-GitHub-Api-Version", "2022-11-28").
-		SetAuthToken(os.Getenv("GITHUB_TOKEN")).
-    	Get("https://api.github.com/repos/neovim/neovim/pulls")
-	
-	if err != nil {
-		fmt.Println(err)
-	}
+	resp := send_request(client, 1, "neovim", "neovim")
+	body := string(resp.Body())
 
-	fmt.Println(resp)
+	r, _ := regexp.Compile(`\"body\":\"([^\"])*\"`)
+	fmt.Println(r.FindAllString(body, -1))
 }
